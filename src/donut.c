1fb0a364ee733d665f483d68eab046405a451610
Peugeot205GTI <clovis.dubois@epitech.eu>
2026-02-25 12:27:42 +0100

    [+] add donut function

---
+++ src/donut.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <string.h>
+
+void donut(void)
+{
+    float z[1760];
+    char b[1760];
+    float A = 0;
+    float B = 0;
+    float c = 1;
+    float d = 0;
+    float e = 1;
+    float a = 0;
+    float g;
+    float h;
+    float G;
+    float H;
+    float t;
+    float f;
+    float D;
+    float A2;
+    int i;
+    int j;
+    int k;
+    int x;
+    int y;
+    int o;
+    int N;
+
+    for (;;) {
+        memset(b, ' ', 1760);
+        memset(z, 0, sizeof(z));
+        g = 0;
+        h = 1;
+        for (j = 0; j < 90; j++) {
+            G = 0;
+            H = 1;
+            for (i = 0; i < 314; i++) {
+                A2 = h + 2;
+                D = 1.0f / (G * A2 * a + g * e + 5.0f);
+                t = G * A2 * e - g * a;
+                x = 40 + (int)(30 * D * (H * A2 * d - t * c));
+                y = 12 + (int)(15 * D * (H * A2 * c + t * d));
+                o = x + 80 * y;
+                N = 8 * ((g * a - G * h * e) * d
+                    - G * h * a
+                    - g * e
+                    - H * h * c);
+                if (y > 0 && y < 22 && x > 0 && x < 80 && D > z[o]) {
+                    z[o] = D;
+                    b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
+                }
+                f = H;
+                H -= 0.02f * G;
+                G += 0.02f * f;
+                f = (3 - H * H - G * G) / 2;
+                H *= f;
+                G *= f;
+            }
+            f = h;
+            h -= 0.07f * g;
+            g += 0.07f * f;
+            f = (3 - h * h - g * g) / 2;
+            h *= f;
+            g *= f;
+        }
+        for (k = 0; k < 1760; k++) {
+            putchar(k % 80 ? b[k] : '\n');
+        }
+        f = e;
+        e -= 0.04f * a;
+        a += 0.04f * f;
+        f = (3 - e * e - a * a) / 2;
+        e *= f;
+        a *= f;
+        f = d;
+        d -= 0.02f * c;
+        c += 0.02f * f;
+        f = (3 - d * d - c * c) / 2;
+        d *= f;
+        c *= f;
+        printf("\n donut.c! \x1b[23A");
+    }
+}
